28a5378b506a2485ae44475f3ea2b9ba9ba19c8f
Michael Gerullis <michigerullis92@gmail.com>
2024-08-02 16:45:37 +0200

    typing

--- src/types.ts
+++ src/types.ts
@@ -3,38 +3,38 @@ export type SSLCertificateCheckerOptions = {
   fingerprint: string;
 };
 
-export type SSLCertificateCheckerResult =
-  | {
-      /**
-       * The subject of the certificate
-       * @platform Android
-       */
-      subject?: string;
-      /**
-       * The issuer of the certificate
-       * @platform Android
-       */
-      issuer?: string;
-      /**
-       * The valid from date of the certificate
-       * @platform Android
-       */
-      validFrom?: string;
-      /**
-       * The valid to date of the certificate
-       * @platform Android
-       */
-      validTo?: string;
-      /**
-       * The fingerprint of the certificate
-       * @platform Android
-       */
-      fingerprint?: string;
-      /**
-       * Whether the fingerprint matches the expected fingerprint
-       */
-      fingerprintMatched: boolean;
-    }
-  | {
-      error: string;
-    };
+export type SSLCertificateCheckerResult = {
+  /**
+   * The subject of the certificate
+   * @platform Android
+   */
+  subject?: string;
+  /**
+   * The issuer of the certificate
+   * @platform Android
+   */
+  issuer?: string;
+  /**
+   * The valid from date of the certificate
+   * @platform Android
+   */
+  validFrom?: string;
+  /**
+   * The valid to date of the certificate
+   * @platform Android
+   */
+  validTo?: string;
+  /**
+   * The fingerprint of the certificate
+   * @platform Android
+   */
+  fingerprint?: string;
+  /**
+   * Whether the fingerprint matches the expected fingerprint
+   */
+  fingerprintMatched?: boolean;
+  /**
+   * The error that occurred while checking the certificate
+   */
+  error?: string;
+};
